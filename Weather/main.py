a91e88877893a553f2b8808ce9a1bd71dd468ded
Sumon Ahmed <85849219+Sumonsiu01@users.noreply.github.com>
2024-06-07 03:06:38 +0600

    new

---
+++ Weather/main.py
@@ -0,0 +1,101 @@
+
+
+import tkinter
+from tkinter import *
+
+root = Tk()
+
+root.title("ToDo App")
+root.geometry("400x650+400+100")
+root.resizable(False, False)
+
+task_list = []
+
+
+def addTask():
+    task = task_entry.get()
+    task_entry.delete(0,END)
+    if task:
+        with open("tasklist.txt",'a') as taskfile:
+            taskfile.write(f"\n{task}")
+        task_list.append(task)
+        listbox.insert(END,task)
+
+def deleteTask():
+    global  task_list
+    task = str(listbox.get(ANCHOR))
+    if task in task_list:
+        task_list.remove(task)
+        with open("tasklist.txt",'w') as taskfile:
+            for task in task_list:
+                taskfile.write(task+"\n")
+        listbox.delete(ANCHOR)
+def openTaskFile():
+
+    try:
+        global task_list
+        with open("tasklist.txt","r") as taskfile:
+             tasks = taskfile.readlines()
+        for task in tasks:
+          if task !='\n':
+                listbox.insert(END,task);
+    except:
+        file = open('tasklist.txt','w')
+        file.close()
+
+#icon
+Image_icon = PhotoImage(file="emg/task.png")
+root.iconphoto(False,Image_icon)
+
+#topbar
+TopImage = PhotoImage(file ="emg/topbar.png")
+Label(root,image=TopImage).pack()
+
+docImage = PhotoImage(file="emg/dock.png")
+Label(root,image=docImage,background="#32405b").place(x=30,y=25)
+
+NotImage = PhotoImage(file="emg/task.png")
+Label(root,image=NotImage,background="#32405b").place(x=30,y=25)
+heading = Label(root,text="ALL TASK",font="arial 20 bold",foreground="white",background="#32405b")
+heading.place(x = 130,y = 20)
+
+#main
+
+frame = Frame(root,width=400,height=50,background="white")
+frame.place(x=0,y=180)
+
+task = StringVar()
+task_entry = Entry(frame,width=18,font='arial 20',border=0)
+task_entry.place(x=10,y=7)
+task_entry.focus()
+
+button = Button(frame,text="ADD",font="arial 20 bold",width=6,background="#5a95ff",foreground="#fff",border=0,command=addTask)
+button.place(x = 300,y = 0)
+
+#list
+
+frame1 = Frame(root,border=3,width=700,height=280,background="#32405b")
+frame1.pack(pady = (160,0))
+listbox = Listbox(frame1,font=('arial',12),width=40,height=16,background="#32405b",foreground="white",cursor="hand2",selectbackground="#5a95ff")
+
+listbox.pack(side = LEFT,fill = BOTH,padx = 2)
+
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side = RIGHT,fill = BOTH)
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
+openTaskFile()
+#delete
+Delete_icon= PhotoImage(file="emg/delete.png")
+Button(root,image=Delete_icon,border=0,command=deleteTask).pack(side = BOTTOM,pady = 13)
+
+
+
+
+
+
+
+
+root.mainloop()
